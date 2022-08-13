2a0cec9bc208d089eef0709cca915ce057564a89
임채성 <pan1129@naver.com>
2022-08-13 17:19:26 +0900

    메인화면 commit

---
+++ src/main/java/com/books/controller/BooksController.java
@@ -0,0 +1,21 @@
+package com.books.controller;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@Controller
+public class BooksController {
+
+    @GetMapping("/")
+    public String main(){
+
+        log.info("******* 메인 화면 접속 **********");
+
+        return "main";
+    }
+
+
+}
